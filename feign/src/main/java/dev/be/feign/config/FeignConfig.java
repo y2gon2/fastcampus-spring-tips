ba4105ea3054a5c47a8bdd37422db70c6054e90d
gon <y2gon2@gmail.com>
2023-11-12 14:48:05 +0900

    #5- Feign Logger
    client 간 통신 시 logging 처리하는 것도 중요하다. Feign Logger 기능을 통해 해당 과정을 구현 및 확인한다.
    
    FeignConfig
     - FeginCustomLogger 를 bean method 로 생성한다. 그런데 interceptor 와 달리, 해당 bean method 는 전역 영역 cofing 로 선언한다. 일반적으로 logging 처리는 전역적으로 처리되기 때문에 해당 config 에 구현 함.
    
    FeignCustomLogger
     - 해당 class 에는  log format 을 정의하는  log method, request log 만 처리 가능한 logRequest, request/response log 처리 모두 가능한  logAndRebufferResponse method 를 override 구현 함.

--- feign/src/main/java/dev/be/feign/config/FeignConfig.java
+++ feign/src/main/java/dev/be/feign/config/FeignConfig.java
@@ -1,8 +1,20 @@
 package dev.be.feign.config;
 
+import dev.be.feign.feign.logger.FeignCustomLogger;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * 전역적 설정을 정의하기 위한 config class
+ * */
 @Configuration
 public class FeignConfig {
 
+    // logger 의 경우 일반적으로 전역적 설정에 반영
+    @Bean
+    public Logger feignLogger() {
+        return new FeignCustomLogger();
+    }
 }
--- feign/src/main/java/dev/be/feign/feign/config/DemoFeignConfig.java
+++ feign/src/main/java/dev/be/feign/feign/config/DemoFeignConfig.java
@@ -4,6 +4,11 @@ import dev.be.feign.feign.interceptor.DemoFeignInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * `feign` floder 하위 내부에 존재하는 FeignClient 를 위한 설정 조건을
+ *  정의하기 위한 config class
+ * */
 @Configuration
 public class DemoFeignConfig {
 
